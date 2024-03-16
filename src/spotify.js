0f297df505f0b855fb30333e8bfb9e385fe65917
MichaelNiekamp <michael.niekamp@umconnect.umt.edu>
2024-03-16 13:51:12 -0600

    fixing redirect

--- src/spotify.js
+++ src/spotify.js
@@ -2,11 +2,11 @@ import axios from "axios";
 
 
 const authEndpoint = "https://accounts.spotify.com/authorize?";
-const redirectUri = "https://newtest-bdfb7.web.app";
+const redirectUri = "https://newtest-bdfb7.web.app/callback";
 
 const scopes = ["user-library-read", "playlist-read-private"];
 
-export const loginEndpoint = `${authEndpoint}client_id=${process.env.REACT_APP_CLIENTID}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`;
+export const loginEndpoint = `${authEndpoint}client_id=${process.env.REACT_APP_CLIENTID}&redirect_uri=https://newtest-bdfb7.web.app/callback&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`;
 
 const apiClient = axios.create({
     baseURL: "https://api.spotify.com/v1/",
